055f5bf10b5348e53abfc06745cbc8231c14e309
anis-aleshatech <84322329+anis-aleshatech@users.noreply.github.com>
2021-08-29 12:12:48 +0600

    Update tgm-activation.php

--- inc/tgm/tgm-activation.php
+++ inc/tgm/tgm-activation.php
@@ -13,9 +13,9 @@ function aleshatech_register_required_plugins() {
 
 		// This is an example of how to include a plugin bundled with a theme.
 		array(
-			'name'               => 'Alesha Tech Elementor Widget', // The plugin name.
-			'slug'               => 'Atl-Elementor-Extension', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/inc/plugins/Atl-Elementor-Extension.zip', // The plugin source.
+			'name'               => 'Alesha Tech Assets', // The plugin name.
+			'slug'               => 'alesha-assets', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/inc/plugins/alesha-assets.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
@@ -23,17 +23,7 @@ function aleshatech_register_required_plugins() {
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
-		array(
-			'name'               => 'Social Icon', // The plugin name.
-			'slug'               => 'socialicon', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/inc/plugins/socialicon.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
+	
 		array(
 			'name'               => 'Revulation Slider', // The plugin name.
 			'slug'               => 'revslider', // The plugin slug (typically the folder name).
