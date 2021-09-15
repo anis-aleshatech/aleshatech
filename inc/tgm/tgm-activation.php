559192eabb31facef3f632f55169f3df0d8f89e9
anis-aleshatech <84322329+anis-aleshatech@users.noreply.github.com>
2021-09-15 18:27:58 +0600

    Update tgm-activation.php

--- inc/tgm/tgm-activation.php
+++ inc/tgm/tgm-activation.php
@@ -71,7 +71,26 @@ function aleshatech_register_required_plugins() {
 		),
 
 	);
-
+	$config = array(
+		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+		/*
+		'strings'      => array(
+			'page_title'                      => __( 'Install Required Plugins', 'theme-slug' ),
+			'menu_title'                      => __( 'Install Plugins', 'theme-slug' ),
+			// <snip>...</snip>
+			'nag_type'                        => 'updated', // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+		)
+		*/
+	);
 	
 
 
